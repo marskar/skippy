3049aa5a96a28198380bffe20429eaf217ed3993
Martin Skarzynski <marskar@gmail.com>
2018-09-29 19:22:08 -0400

    deploy 004

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skippy",
-    version="0.0.3",
+    version="0.0.4",
     author="Martin Skarzynski",
     author_email="marskar@gmail.com",
     description="Simplified analysis of sklearn datasets",
